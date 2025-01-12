57fd7862d18d6acf67f6b1fbe090cea67c995878
Kirill190611 <k.igorevitch88@yandex.ru>
2025-01-12 20:10:29 +0300

    Solution for leetcode tasks 'length of last word'

---
+++ Easy/10. LengthOfLastWord.js
@@ -0,0 +1,46 @@
+//Task: Given a string s consisting of words and spaces, return the length of the last word in the string.
+//
+// A word is a maximal
+// substring
+//  consisting of non-space characters only.
+//
+//
+//
+// Example 1:
+//
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+//
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+//
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+//
+//
+// Constraints:
+//
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+
+//Link: https://leetcode.com/problems/length-of-last-word/description/
+
+
+//Solution:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = s => {
+    const sWithOutWhiteSpaces = s.trim();
+    const wordsInS = sWithOutWhiteSpaces.split(" ");
+    const lastWord = wordsInS[wordsInS.length - 1];
+    return lastWord.length;
+};
